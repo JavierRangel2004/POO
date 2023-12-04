583ce9313ad4ce951e90713daac6a86081240d15
JavierRangel2004 <0256158@up.edu.mx>
2023-12-03 19:00:32 -0600

    added requirements, and reomed sepia for new noise funct

--- Final_PDI/procesamiento_imagenes/views.py
+++ Final_PDI/procesamiento_imagenes/views.py
@@ -24,12 +24,6 @@ def assure_rgb(imagen):
 def reducir_ruido(imagen):
     """
     Reduce el ruido de una imagen utilizando un filtro bilateral.
-
-    Args:
-    imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
-
-    Returns:
-    np.ndarray: Imagen con ruido reducido.
     """
     imagen_rgb = assure_rgb(imagen)
     imagen_sin_ruido = cv.bilateralFilter(imagen_rgb, 9, 75, 75)
@@ -39,12 +33,6 @@ def reducir_ruido(imagen):
 def aplicar_filtro_sepia(imagen):
     """
     Aplica un filtro sepia a una imagen.
-
-    Args:
-    imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
-
-    Returns:
-    np.ndarray: Imagen con filtro sepia.
     """
     imagen_rgb = assure_rgb(imagen)
     imagen_sepia = np.dot(imagen_rgb, [[0.272, 0.534, 0.131],
@@ -92,9 +80,6 @@ def binarizar_imagen(imagen, umbral=127):
     Args:
     imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
     umbral (int): Umbral para la binarización. Valor por defecto es 127.
-
-    Returns:
-    np.ndarray: Imagen binarizada.
     """
     imagen_rgb = assure_rgb(imagen)
     _, imagen_binarizada = cv.threshold(cv.cvtColor(
@@ -105,12 +90,6 @@ def binarizar_imagen(imagen, umbral=127):
 def negativo_imagen(imagen):
     """
     Crea el negativo de una imagen.
-
-    Args:
-    imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
-
-    Returns:
-    np.ndarray: Negativo de la imagen.
     """
     imagen_rgb = assure_rgb(imagen)
     imagen_negativa = 255 - imagen_rgb
@@ -124,9 +103,6 @@ def transformacion_logaritmica(imagen, constante=1):
     Args:
     imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
     constante (float): Constante para ajustar la transformación. Valor por defecto es 1.
-
-    Returns:
-    np.ndarray: Imagen con transformación logarítmica.
     """
     imagen_rgb = assure_rgb(imagen)
     c = constante * 255 / np.log(1 + np.max(imagen_rgb))
@@ -138,12 +114,6 @@ def transformacion_logaritmica(imagen, constante=1):
 def histograma_imagen(imagen):
     """
     Calcula y muestra el histograma combinado de los canales RGB de una imagen.
-
-    Args:
-    imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
-
-    Returns:
-    np.ndarray: Imagen de los histogramas combinados.
     """
     imagen_rgb = assure_rgb(imagen)
     colores = ('r', 'g', 'b')
@@ -176,9 +146,6 @@ def filtrar_suavizar_imagen(imagen, kernel_size=5):
     Args:
     imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
     kernel_size (int): Tamaño del kernel para el filtro. Valor por defecto es 5.
-
-    Returns:
-    np.ndarray: Imagen suavizada.
     """
     imagen_rgb = assure_rgb(imagen)
     imagen_suavizada = cv.GaussianBlur(
@@ -195,15 +162,32 @@ def umbralizacion_adaptativa(imagen, max_value=255, block_size=11, C=2):
     max_value (int): Valor máximo a utilizar en la umbralización. Valor por defecto es 255.
     block_size (int): Tamaño del bloque para calcular el umbral. Valor por defecto es 11.
     C (int): Constante a restar del promedio o suma ponderada. Valor por defecto es 2.
-
-    Returns:
-    np.ndarray: Imagen con umbralización adaptativa.
     """
     imagen_rgb = assure_rgb(imagen)
     imagen_gris = cv.cvtColor(imagen_rgb, cv.COLOR_RGB2GRAY)
     imagen_umbralizada = cv.adaptiveThreshold(
         imagen_gris, max_value, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, block_size, C)
     return imagen_umbralizada
+
+def resaltar_diferencias_imagen(imagen):
+    """
+    Resalta las diferencias en una imagen aplicando un filtro de mediana y 
+    calculando la diferencia con la imagen original.
+
+    Args:
+    imagen (np.ndarray): Imagen de entrada en formato de matriz numpy.
+
+    Returns:
+    np.ndarray: Imagen con diferencias resaltadas.
+    """
+    imagen=assure_rgb(imagen)
+    
+    imagen_gris = cv.cvtColor(imagen, cv.COLOR_BGR2GRAY)
+    imagen_filtrada = cv.medianBlur(imagen_gris, 5)
+    diferencia = cv.subtract(imagen_filtrada, imagen_gris)
+    diferencia = cv.cvtColor(diferencia, cv.COLOR_GRAY2BGR)
+    imagen_mejorada = cv.add(imagen, diferencia)
+    return imagen_mejorada
 
 
 def image_upload_view(request):
@@ -227,7 +211,7 @@ def image_upload_view(request):
                 try:
                     image_sin_ruido = reducir_ruido(image)
                     image_mejorada = mejorar_calidad(image)
-                    image_sepia = aplicar_filtro_sepia(image)
+                    image_diferencias_resaltadas = resaltar_diferencias_imagen(image)
                     image_BGR = convertir_a_BGR(image)
                     image_bnw = convertir_a_bnw(image)
                     image_binarizada = binarizar_imagen(image)
@@ -242,13 +226,12 @@ def image_upload_view(request):
                         'original': file_url,
                         'sin_ruido': save_filter_image(fs, image_sin_ruido, 'sin_ruido_' + file_name),
                         'mejorada': save_filter_image(fs, image_mejorada, 'mejorada_' + file_name),
-                        'sepia': save_filter_image(fs, image_sepia, 'sepia_' + file_name),
+                        'diferencias_resaltadas': save_filter_image(fs, image_diferencias_resaltadas, 'diferencias_resaltadas_' + file_name),
                         'BGR': save_filter_image(fs, image_BGR, 'BGR_' + file_name),
                         'bnw': save_filter_image(fs, image_bnw, 'bnw_' + file_name),
                         'binarizada': save_filter_image(fs, image_binarizada, 'binarizada_' + file_name),
                         'negativa': save_filter_image(fs, image_negativa, 'negativa_' + file_name),
                         'log': save_filter_image(fs, image_log, 'log_' + file_name),
-                        # Needs additional processing
                         'histograma': save_filter_image(fs, image_histograma, 'histograma_' + file_name),
                         'suavizada': save_filter_image(fs, image_suavizada, 'suavizada_' + file_name),
                         'umbralizada': save_filter_image(fs, image_umbralizada, 'umbralizada_' + file_name),
